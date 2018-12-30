6009224ce67aff36963972dee772284ea5de6a87
Shalltearz <42479034+Tera-Shiraneko@users.noreply.github.com>
2018-12-31 00:33:23 +0100

    Add files via upload

---
+++ settings_structure.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        "key": "autotrash",
+        "name": "Automatic discarding of items in your trashlist.",
+        "type": "bool"
+    },
+    {
+        "key": "trashlist",
+        "name": "Here you can add item id's to the trashlist.",
+        "type": "string"
+    }
+];
